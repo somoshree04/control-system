60e2d3ac6111bd3838f8d446c6eb7adda37d85ab
somoshree04 <somoshreesaha77@gmail.com>
2026-07-07 02:37:03 +0530

    added 2nd and 3rd controller codes and images of the plots

---
+++ PD Controllers/2nd_controller.m
@@ -0,0 +1,28 @@
+﻿
+pkg load control
+
+kp=30;
+ki=70;
+
+n=[kp ki];
+d=[1 10 20+kp ki];
+
+t=0:0.01:2;
+
+step(n,d,t);
+grid on;
+
+%{
+for matlab
+
+kp=30;
+ki=70;
+
+n=[kp ki];
+d=[1 10 20+kp ki];
+
+t=0:0.01:2;
+
+step(n,d,t);
+
+%}
---
+++ PD Controllers/3rd_controller.m
@@ -0,0 +1,17 @@
+﻿
+pkg load control
+
+kp=350;
+ki=300;
+kd=50;
+
+n=[kd kp ki];
+d=[1 10+kd 20+kp ki];
+
+sys= tf(n,d);
+
+t=0:0.1:2;
+
+step(sys,t);
+
+grid on;
